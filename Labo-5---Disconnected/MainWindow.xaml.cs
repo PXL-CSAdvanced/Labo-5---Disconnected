41e740fd57d4c4b88ec503b8c2eefffa769fa2fb
Sander De Puydt <sdp2718@gmail.com>
2025-03-20 00:42:28 +0100

    opdracht labo

---
+++ Labo-5---Disconnected/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MonsterClassLibrary.DataAccess;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labo_5___Disconnected
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MonsterDataGrid.ItemsSource = MonsterData.MonsterDataTable.DefaultView;
+        }
+
+        private void ClearMonsters_Click(object sender, RoutedEventArgs e)
+        {
+            MonsterData.ClearAllMonsters();
+        }
+
+        private void ImportFromXml_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportToXml_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateMonster_Click(object sender, RoutedEventArgs e)
+        {
+            MonsterData.AddRandomMonster();
+        }
+    }
+}
---
+++ MonsterClassLibrary/DataAccess/MonsterData.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MonsterClassLibrary.Entities;
+
+namespace MonsterClassLibrary.DataAccess
+{
+    public static class MonsterData
+    {
+        private static DataSet _monsterDataSet = new DataSet();
+        private static DataTable _monsterDataTable;
+        public static DataTable MonsterDataTable { get { return _monsterDataTable; } }
+        private static DataTable _tierDataTable;
+
+        private static Random _randomizer = new Random();
+
+        static MonsterData()
+        {
+            CreateMonsterDataTable();
+            CreateTierDataTable();
+            FillTierDataTable();
+
+            _monsterDataSet.Tables.Add(_monsterDataTable);
+            _monsterDataSet.Tables.Add(_tierDataTable);
+        }
+
+
+        private static void CreateMonsterDataTable()
+        {
+            // throw new NotImplementedException();
+            
+            _monsterDataTable = new DataTable("Monsters");
+            DataColumn dcId = new DataColumn("Id", typeof(int));
+            _monsterDataTable.Columns.Add(dcId);
+            _monsterDataTable.Columns.Add("Name", typeof(string));
+            _monsterDataTable.Columns.Add("Tier", typeof(string));
+            _monsterDataTable.Columns.Add("BaseHealth", typeof(int));
+            _monsterDataTable.Columns.Add("BasePower", typeof(int));
+
+            UniqueConstraint unique = new UniqueConstraint(dcId);
+            _monsterDataTable.Constraints.Add(unique);
+            _monsterDataTable.PrimaryKey = new DataColumn[] { dcId };
+        }
+
+        private static void CreateTierDataTable()
+        {
+            // throw new NotImplementedException();
+
+            _tierDataTable = new DataTable("Tiers");
+            DataColumn dcName = new DataColumn("Name", typeof(string));
+            _tierDataTable.Columns.Add(dcName);
+            _tierDataTable.Columns.Add("Modifier", typeof(double));
+            
+            _tierDataTable.PrimaryKey = new DataColumn[] {dcName};
+        }
+        
+        private static void FillTierDataTable()
+        {
+            string[] tiers = { "Baby", "Battle", "Adult", "Ancient", "Astral", "Godlike", "Transcendental", "Meta Infinite" };
+            double[] modifiers = { 1, 1.2, 1.5, 2, 3, 10, 20, 1000 };
+            // throw new NotImplementedException();
+
+            for (int i = 0; i < tiers.Length; i++)
+            {
+                _tierDataTable.Rows.Add(tiers[i], modifiers[i]);
+            }
+        }
+
+        public static void AddRandomMonster() 
+        {
+            Monster randomMonster = MonsterGenerateRandomMonster();
+            // TODO: Voeg het willekeurige monster toe aan _monsterDataTable
+
+            _monsterDataTable.Rows.Add(randomMonster.Id, randomMonster.Name, randomMonster.Tier,
+                randomMonster.BaseHealth, randomMonster.BasePower);
+        }
+
+        public static void ClearAllMonsters()
+        {
+            _monsterDataTable.Rows.Clear();
+        }
+
+        private static Monster MonsterGenerateRandomMonster()
+        {
+            string[] names = { "Vampire", "Werewolf", "Zombie", "Dragon", "Goblin", "Troll", "Demon", "Couatl" };
+            List<Tier> tiers = _tierDataTable.AsEnumerable().Select(x => new Tier(x)).ToList();
+            Monster randomMonster = new Monster(
+                _monsterDataTable.Rows.Count + 1,
+                names[_randomizer.Next(names.Length)],
+                tiers[_randomizer.Next(tiers.Count)].Name,
+                _randomizer.Next(10, 100),
+                _randomizer.Next(10, 100));
+            return randomMonster;
+        }
+
+    }
+}
